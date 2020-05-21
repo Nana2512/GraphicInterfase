9ba151e98eada9b70b05776e073eb0bacd9143d3
as259 <a.o.s.79.21.52@gmail.com>
2020-05-22 01:50:14 +0300

    Initial commit

---
+++ p004linearlayout/src/main/java/com/example/p004linearlayout/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.p004linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void sendMessage(View view) {
+        TextView textView=(TextView) findViewById(R.id.textView1);
+        textView.setText("hello world");
+    }
+}
